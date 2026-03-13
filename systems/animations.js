6f13dc67b611b088974faec1fcc3170ab25b623a
subhashithv <subhashithv@gmail.com>
2026-03-13 21:52:51 +0530

    Animation update

--- systems/animations.js
+++ systems/animations.js
@@ -1,9 +1,10 @@
 const animations = {
 
-blackPunch: "https://media1.tenor.com/m/-nZnQBzGa7EAAAAd/jujutsu-kaisen-jujutsu-kaisen-season-2.gif",
-whitePunch: "https://media1.tenor.com/m/kBvBhMiZRMwAAAAd/boxing-anime.gif",
-dodge: "https://media1.tenor.com/m/aRajiEaHEPoAAAAd/dbz-goku.gif",
-block: "https://cdn.discordapp.com/attachments/1411680226332905554/1482024742020972544/image0.gif?ex=69b57299&is=69b42119&hm=1599ff6340be613c35e915de0057fe688deb9760ea7cb7d71a84d6a629df44a9",
+blackPunch: "https://media.tenor.com/-nZnQBzGa7EAAAAd/tenor.gif",
+whitePunch: "https://media.tenor.com/kBvBhMiZRMwAAAAd/tenor.gif",
+
+dodge: "https://media.tenor.com/aRajiEaHEPoAAAAd/tenor.gif",
+block: "https://cdn.discordapp.com/attachments/1411680226332905554/1482024742020972544/image0.gif",
 
 attackerWin: "https://media.tenor.com/uEGlMO15H6AAAAAM/kira.gif",
 defenderWin: "https://media.tenor.com/uEGlMO15H6AAAAAM/kira.gif",
